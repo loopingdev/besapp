c24f84c0bc3eb60c7bb14205ec3fbdc269fc203c
loopingdev <edwinvarenoyp@gmail.com>
2021-03-31 17:54:20 +0700

    Update index.tsx

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -37,8 +37,8 @@ class FooterComponent extends React.Component<FooterProps> {
         return (
             <React.Fragment>
                 <footer className="pg-footer">
-                    <span>{this.translate('pagy.body.footer.powered_by')}</span>
-                    <a href="https://www.openware.com">openware.com</a>
+                    <span></span>
+                    <a href=""></a>
                 </footer>
             </React.Fragment>
         );
